34a946bfd869f22c2cf8261b087b6faa9d51ce78
ajay-chandravanshi <ajaystudy38@gmail.com>
2025-11-21 00:27:44 +0530

    first commit

---
+++ import_and_export_js/main.js
@@ -0,0 +1,10 @@
+import chacha from './second.js'
+import a from './second.js'
+import c from './third.js'
+import {b} from './third.js'
+
+
+console.log(chacha)
+console.log(c)
+console.log(b)
+console.log(a)
---
+++ import_and_export_js/third.js
@@ -0,0 +1,5 @@
+export const b=20+30
+
+const c=45
+
+export default c
